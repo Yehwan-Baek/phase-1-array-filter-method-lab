57f2670bca716ed8be2d05dbcb52f0953de709fe
Yehwan Baek <baekokok98@gmail.com>
2023-03-13 22:09:00 +1100

    added code

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, driversName){
+    let match = collection.filter(
+        array => array.toLowerCase() === driversName.toLowerCase()
+        )
+    if (match){
+        return match
+    } else{return undefined}    
+}
+
+function fuzzyMatch(collection, letters){    
+    let lettersMatch = collection.filter(
+        array => array.toLowerCase().indexOf(letters.toLowerCase())===0
+        )
+    return lettersMatch;
+}
+
+console.log(fuzzyMatch(drivers, "Sa"))
+
+const driversObj = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(collection, driverName){
+    return collection.filter(
+        obj=>obj.name === driverName)
+   
+}
+
+console.log(matchName(driversObj, "Bobby"))
